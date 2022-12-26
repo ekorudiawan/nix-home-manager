b4672e116d86e7caa09af3c1b6798ec7c764b4e3
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2022-12-26 11:35:31 +0700

    add packages

--- home.nix
+++ home.nix
@@ -6,6 +6,43 @@
   home.username = "administrator";
   home.homeDirectory = "/home/administrator";
 
+  # Packages to install
+  home.packages = [
+    # pkgs is the set of all packages in the default home.nix implementation
+    pkgs.fish
+    pkgs.git
+    pkgs.tmux
+    pkgs.neovim
+    pkgs.bat
+    pkgs.exa
+    pkgs.lsd
+    pkgs.delta
+    pkgs.duf
+    pkgs.broot
+    pkgs.fd
+    pkgs.ripgrep
+    pkgs.fzf
+    pkgs.mcfly
+    pkgs.choose
+    pkgs.jq
+    pkgs.sd
+    pkgs.cheat
+    pkgs.tldr
+    pkgs.bottom
+    pkgs.glances
+    pkgs.gtop
+    pkgs.hyperfine
+    pkgs.gping
+    pkgs.procs
+    pkgs.httpie
+    pkgs.curlie
+    pkgs.xh
+    pkgs.zoxide
+    pkgs.dog
+  ];
+
+  programs.fish.enable = true;
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
