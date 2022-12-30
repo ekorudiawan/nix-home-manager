80095050becc2690caa4df3bb813014a63c02bd6
Eko Rudiawan Jamzuri <eko.rudiawan@gmail.com>
2022-12-30 14:47:15 +0700

    change tabsize

--- home.nix
+++ home.nix
@@ -1,67 +1,66 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "ekorudiawan";
-  home.homeDirectory = "/home/ekorudiawan";
+	# Home Manager needs a bit of information about you and the
+	# paths it should manage.
+	home.username = "ekorudiawan";
+	home.homeDirectory = "/home/ekorudiawan";
 
-  # Packages to install
-  home.packages = [
-    # pkgs is the set of all packages in the default home.nix implementation
-    pkgs.neofetch
-    pkgs.wget
-    pkgs.curl
-    pkgs.fish
-    pkgs.git
-    pkgs.tmux
-    pkgs.neovim
-    pkgs.bat
-    pkgs.exa
-    pkgs.lsd
-    pkgs.delta
-    pkgs.duf
-    pkgs.broot
-    pkgs.fd
-    pkgs.ripgrep
-    pkgs.fzf
-    pkgs.mcfly
-    pkgs.choose
-    pkgs.jq
-    pkgs.sd
-    pkgs.cheat
-    pkgs.tldr
-    pkgs.bottom
-    pkgs.glances
-    pkgs.gtop
-    pkgs.hyperfine
-    pkgs.gping
-    pkgs.procs
-    pkgs.httpie
-    pkgs.curlie
-    pkgs.xh
-    pkgs.zoxide
-    pkgs.dog
-    pkgs.nix-prefetch-github
-  ];
+	# Packages to install
+	home.packages = [
+		# pkgs is the set of all packages in the default home.nix implementation
+		pkgs.neofetch
+		pkgs.wget
+		pkgs.curl
+		pkgs.fish
+		pkgs.git
+		pkgs.tmux
+		pkgs.neovim
+		pkgs.bat
+		pkgs.exa
+		pkgs.lsd
+		pkgs.delta
+		pkgs.duf
+		pkgs.broot
+		pkgs.fd
+		pkgs.ripgrep
+		pkgs.fzf
+		pkgs.mcfly
+		pkgs.choose
+		pkgs.jq
+		pkgs.sd
+		pkgs.cheat
+		pkgs.tldr
+		pkgs.bottom
+		pkgs.glances
+		pkgs.gtop
+		pkgs.hyperfine
+		pkgs.gping
+		pkgs.procs
+		pkgs.httpie
+		pkgs.curlie
+		pkgs.xh
+		pkgs.zoxide
+		pkgs.dog
+		pkgs.nix-prefetch-github
+	];
 
-  programs.fish = {
-    enable = true;
-    shellInit =
-    ''
-    #.  /home/ekorudiawan/.nix-profile/etc/profile.d/nix.fish
-    '';
-		shellAliases = {
-			# rm = "rm -i";
-			# cp = "cp -i";
-			# mv = "mv -i";
-			# mkdir = "mkdir -p";
-			cat = "bat";
-			ls = "exa";
-			vim = "nvim";
-			ping = "gping";
-		};
-    plugins = [
+	programs.fish = {
+		enable = true;
+		shellInit =
+		''
+		'';
+			shellAliases = {
+				# rm = "rm -i";
+				# cp = "cp -i";
+				# mv = "mv -i";
+				# mkdir = "mkdir -p";
+				cat = "bat";
+				ls = "exa";
+				vim = "nvim";
+				ping = "gping";
+			};
+		plugins = [
 			{
 				name = "nix-env";
 				src = pkgs.fetchFromGitHub {
@@ -71,7 +70,7 @@
 					sha256 = "1hrl22dd0aaszdanhvddvqz3aq40jp9zi2zn0v1hjnf7fx4bgpma";
 				};
 			}
-      {
+			{
 				name = "foreign-env";
 				src = pkgs.fetchFromGitHub {
 					owner = "oh-my-fish";
@@ -79,30 +78,21 @@
 					rev = "dddd9213272a0ab848d474d0cbde12ad034e65bc";
 					sha256 = "00xqlyl3lffc5l0viin1nyp819wf81fncqyz87jx8ljjdhilmgbs";
 				};
-      }
-			# {
-			# 	name = "oh-my-fish";
-			# 	src = pkgs.fetchFromGitHub {
-			# 		owner = "oh-my-fish";
-			# 		repo = "oh-my-fish";
-			# 		rev = "92a572d8cdfdf5b219269d59210b8a28f6cd6616";
-			# 		sha256 = "eCNVdVm33n8gizeYUGvVw0siQPsKKjaWn+adbyKwEYs=";
-			# 	};
-      # }
-    ];
-		
-  };
+			}
+		];
+			
+	};
 
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.11";
+	# This value determines the Home Manager release that your
+	# configuration is compatible with. This helps avoid breakage
+	# when a new Home Manager release introduces backwards
+	# incompatible changes.
+	#
+	# You can update Home Manager without changing this value. See
+	# the Home Manager release notes for a list of state version
+	# changes in each release.
+	home.stateVersion = "22.11";
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
+	# Let Home Manager install and manage itself.
+	programs.home-manager.enable = true;
 }
